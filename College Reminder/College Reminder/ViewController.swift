a1a6da995d07c0a7d2d861587c90ccd732ce9cfb
Alejandro.Casanas <casanas10@ufl.edu>
2015-10-05 10:01:58 -0400

    Hello

---
+++ College Reminder/College Reminder/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  College Reminder
+//
+//  Created by alejandro casanas on 9/25/15.
+//  Copyright (c) 2015 alejandro casanas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
